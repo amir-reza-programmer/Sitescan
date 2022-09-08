bdf0dd950f4a7c51b95a4f4690a619d73535f869
amir-reza-programmer <amirrezazare130081@gmail.com>
2022-09-08 23:21:34 +0430

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import {  Route, Routes, BrowserRouter, Navigate} from "react-router-dom";
+import React, {useState, useEffect} from "react";
+import Home from './components/Home';
 import './App.css';
 
+
 function App() {
+
+  const [width, setWidth] = useState(window.innerWidth);
+  const handleWindowSizeChange = () => {
+    setWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  const isMobile = width <= 768;
+  const isDesk = !isMobile;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+          <Routes>
+            <Route path="home" element={<Home/>}/>
+            <Route path="about"/>
+            <Route path="/" element={<Navigate replace to="home" />} />
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
