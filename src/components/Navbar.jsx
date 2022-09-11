67373226a1168d5df65e02195cc93b1c5c8a2599
amir-reza-programmer <amirrezazare130081@gmail.com>
2022-09-11 22:12:08 +0430

    fix icon import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
-import Icon from "../pics/icon.svg";
+import Icon from "../images/icon.svg";
 
 const Navbar = ({ active }) => {
   const itemStyle = (name, color) => {
