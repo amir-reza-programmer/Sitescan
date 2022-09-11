098e8ff6d2f097fc5b3de6a2f0742fbd26a17276
Mehrab <mehrabgheibi@gmail.com>
2022-09-11 10:49:09 +0430

    Customer section in AboutUsPage is implemented.

--- src/components/Customers.jsx
+++ src/components/Customers.jsx
@@ -1,7 +1,18 @@
-import styles from "./Customers.module.css"
+import customers from "../data_information/customers";
+
+import styles from "./Customers.module.css";
 
 const Customers = () => {
-    return ( <div>Customers</div> );
-}
- 
+  return (
+    <section className={styles.customer_section}>
+      <p className={styles.title}>مشتریان ما</p>
+      <div className={styles.customers}>
+        {customers.map((customer, index) => (
+          <img key={index} src={customer}></img>
+        ))}
+      </div>
+    </section>
+  );
+};
+
 export default Customers;
