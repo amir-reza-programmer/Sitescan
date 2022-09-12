f028324007b4ce0e88bd88347adcd9332635360d
Mehrab <mehrabgheibi@gmail.com>
2022-09-13 00:55:57 +0430

    ProjectsPage is implemented.

--- src/components/mainPages/ProjectsPage/ProjectsSection.jsx
+++ src/components/mainPages/ProjectsPage/ProjectsSection.jsx
@@ -3,37 +3,47 @@ import projects from "../../../data_information/projects";
 import styles from "./ProjectsSection.module.css";
 
 const ProjectsSection = () => {
-  const rtlMode = <div></div>;
+  const rtlMode = (project, index) => {
+    return (
+      <div className={styles.project} key={index}>
+        <div>
+          <p className={styles.rtl_header_title}>طراحی سایت</p>
+          <p className={styles.rtl_proj_title}>{project.title}</p>
+          <p className={styles.rtl_proj_text}>{project.text}</p>
+          <button className={styles.rtl_more_info_btn} type="button">
+            جزئیات بیشتر
+          </button>
+        </div>
+        <img src={project.pic}></img>
+      </div>
+    );
+  };
 
-  const ltrMode = <div></div>;
+  const ltrMode = (project, index) => {
+    return (
+      <div className={styles.project} key={index}>
+        <img src={project.pic}></img>
+        <div>
+          <p className={styles.ltr_header_title}>طراحی سایت</p>
+          <p className={styles.ltr_proj_title}>{project.title}</p>
+          <p className={styles.ltr_proj_text}>{project.text}</p>
+          <button className={styles.ltr_more_info_btn} type="button">
+            جزئیات بیشتر
+          </button>
+        </div>
+      </div>
+    );
+  };
 
-  let isEven = 0;
   return (
-    <div>
-      <p>پروژه های ما</p>
+    <div className={styles.projects_section}>
+      <p className={styles.proj_page_title}>پروژه های ما</p>
       <div>
         {projects.map((project, index) => {
-          {
-            isEven = index % 2 === 0;
+          if (index % 2 === 0) {
+            return rtlMode(project, index);
           }
-          {
-            isEven && (
-              <div className={styles.project} key={index}>
-                <div className={styles.proj_description}>
-                  <p className={styles.header_title}>طراحی سایت</p>
-                  <p className={styles.proj_title}>{project.title}</p>
-                  <p className={styles.proj_text}>{project.text}</p>
-                  <button className={styles.more_info_btn} type="button">
-                    جزئیات بیشتر
-                  </button>
-                </div>
-                <img className={styles.pic} src={project.pic}></img>
-              </div>
-            );
-          }
-          {
-            !isEven && <div key={index}></div>;
-          }
+          return ltrMode(project, index);
         })}
       </div>
     </div>
