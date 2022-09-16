73d8e7a272027d67dd55a8eeae197fb1639d620f
Mehrab <mehrabgheibi@gmail.com>
2022-09-16 19:01:57 +0430

    footer component is implemented.

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,7 +1,33 @@
-import styles from "./Footer.moudle.css";
+import styles from "./Footer.module.css";
+
+import sitescanIcon from "../../images/SitescanIcon.svg";
+import linkedin from "../../images/Footer/Linkedin.svg";
+import instagram from "../../images/Footer/Instagram.svg";
+import telegram from "../../images/Footer/Telegram.svg";
+import whatsapp from "../../images/Footer/Whatsapp.svg";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.footer_section}>
+      <div className={styles.head_title}>
+        <img src={sitescanIcon}></img>
+        <p className={styles.title_text}>سایت اسکن</p>
+      </div>
+      <div className={styles.address_number}>
+        <p className={styles.address}>
+          تهران، خیابان کارگر شمالی، پردیس دانشکده‌های فنی دانشگاه تهران
+          <br></br> شتاب‌دهنده اینومکث
+        </p>
+        <p className={styles.number}>021 - 12345678</p>
+      </div>
+      <div>
+        <img className={styles.icon} src={whatsapp}></img>
+        <img className={styles.icon} src={telegram}></img>
+        <img className={styles.icon} src={instagram}></img>
+        <img className={styles.icon} src={linkedin}></img>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
