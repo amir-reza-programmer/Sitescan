7f752383afbf37bdc8a8574b8baa89eafb686767
amir-reza-programmer <amirrezazare130081@gmail.com>
2022-09-20 22:49:09 +0430

    form

--- src/components/common/Form.jsx
+++ src/components/common/Form.jsx
@@ -1,19 +1,51 @@
 import styles from "./Form.module.css";
+import { useState } from "react";
 
 const Form = ({ title }) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredQuestion, setEnteredQuestion] = useState("");
+
+  const nameHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+  const emailHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+  const questionHandler = (event) => {
+    setEnteredQuestion(event.target.value);
+  };
+  const formHandler = () => {
+    if (enteredName.trim().length >= 3 && enteredQuestion.trim().length >= 5) {
+      window.location.reload();
+      console.log("reloaded");
+    }
+  };
   return (
     <div className={styles["form-parent"]}>
       <h2 className={styles.title}>{title}</h2>
-      <form action="">
-        <input type="text" placeholder="نام و نام خانوادگی" />
-        <input type="email" placeholder="آدرس ایمیل" />
+      <form onSubmit={formHandler} action="">
+        <input
+          value={enteredName}
+          onChange={nameHandler}
+          type="text"
+          placeholder="نام و نام خانوادگی"
+        />
+        <input
+          value={enteredEmail}
+          onChange={emailHandler}
+          type="email"
+          placeholder="آدرس ایمیل"
+        />
         <textarea
           name="Text1"
+          value={enteredQuestion}
+          onChange={questionHandler}
           cols="40"
           rows="5"
           placeholder="متن سوال"
         ></textarea>
-        <input type="button" value="ارسال" />
+        <input type="submit" value="ارسال" />
       </form>
     </div>
   );
