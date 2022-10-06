29cf874abb9e43ca6d24ef85188cf5f66bacce1b
AmirReza <amirrezazare130081@gmail.com>
2022-10-06 19:10:37 +0330

    team members mobile

--- src/components/mainPages/AboutUsPage/TeamMembers.jsx
+++ src/components/mainPages/AboutUsPage/TeamMembers.jsx
@@ -1,15 +1,29 @@
 import MemberCard from "./common/MemberCard";
+import { useSelector } from "react-redux";
 import teamMembers from "../../../data_information/teamMembers";
 
 import styles from "./TeamMembers.module.css";
 
 const TeamMembers = () => {
+  const mobileView = useSelector((state) => state.mobile.mobileView);
   return (
     <section className={styles.team_section}>
-      <p className={styles.title}>اعضای تیم ما</p>
-      <div className={styles.team_members}>
+      <p
+        className={styles.title + ` ${mobileView ? styles.title_mobile : null}`}
+      >
+        اعضای تیم ما
+      </p>
+      <div
+        className={
+          mobileView ? styles.team_members_mobile : styles.team_members
+        }
+      >
         {teamMembers.map((member) => (
-          <MemberCard key={member.id} {...member}></MemberCard>
+          <MemberCard
+            mobileView={mobileView}
+            key={member.id}
+            {...member}
+          ></MemberCard>
         ))}
       </div>
     </section>
--- src/components/mainPages/AboutUsPage/common/MemberCard.jsx
+++ src/components/mainPages/AboutUsPage/common/MemberCard.jsx
@@ -4,14 +4,34 @@ import Github from "../../../../images/TeamMembres/Github.svg";
 
 const MemberCard = (props) => {
   const noGithubLink = props.github === "";
-
+  const { mobileView } = props;
   return (
-    <section className={styles.member_card}>
-      <img className={styles.member_img} src={props.pic}></img>
+    <section
+      className={mobileView ? styles.member_card_mobile : styles.member_card}
+    >
+      <img
+        className={
+          styles.member_img + ` ${mobileView ? styles.member_img_mobile : null}`
+        }
+        src={props.pic}
+        alt="profile_pic"
+      ></img>
       <div className={styles.info}>
-        <p className={styles.full_name}>{props.fullname}</p>
-        <p className={styles.position}>{props.position}</p>
-        <div className={styles.links}>
+        <p
+          className={
+            styles.full_name + ` ${mobileView ? styles.text_mobile : null}`
+          }
+        >
+          {props.fullname}
+        </p>
+        <p
+          className={
+            styles.position + ` ${mobileView ? styles.text_mobile : null}`
+          }
+        >
+          {props.position}
+        </p>
+        <div className={mobileView ? styles.links_mobile : styles.links}>
           <a href={props.linkedin}>
             <img src={Linkedin} alt="Linkedin Image"></img>
           </a>
