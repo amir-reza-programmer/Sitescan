9251f5662c5b19aa00ae2e3d2ded71e7a78e7e6a
AmirReza <amirrezazare130081@gmail.com>
2022-10-06 19:17:08 +0330

    projects page mobile

--- src/components/mainPages/ProjectsPage/ProjectsSection.jsx
+++ src/components/mainPages/ProjectsPage/ProjectsSection.jsx
@@ -1,8 +1,9 @@
 import projects from "../../../data_information/projects";
-
+import { useSelector } from "react-redux";
 import styles from "./ProjectsSection.module.css";
 
 const ProjectsSection = () => {
+  const mobileView = useSelector((state) => state.mobile.mobileView);
   const rtlMode = (project) => {
     return (
       <div className={styles.project} key={project.id}>
@@ -35,11 +36,44 @@ const ProjectsSection = () => {
     );
   };
 
+  const mobileViewMode = (project) => {
+    return (
+      <div className={styles.proj_mobile} key={project.id}>
+        <div className={styles.proj_descriptions_mobile}>
+          <p className={styles.header_title_mobile}>طراحی سایت</p>
+          <p className={styles.proj_title_mobile}>{project.title}</p>
+          <p className={styles.proj_text_mobile}>{project.text}</p>
+        </div>
+        <img
+          className={styles.proj_pic_mobile}
+          src={project.pic}
+          alt="project_pic"
+        ></img>
+        <button className={styles.more_info_btn_mobile} type="button">
+          مشاهده وبسایت
+        </button>
+      </div>
+    );
+  };
+
   return (
-    <div className={styles.projects_section}>
-      <p className={styles.proj_page_title}>پروژه های ما</p>
+    <div
+      className={
+        mobileView ? styles.projects_section_mobile : styles.projects_section
+      }
+    >
+      <p
+        className={
+          mobileView ? styles.proj_page_title_mobile : styles.proj_page_title
+        }
+      >
+        پروژه های ما
+      </p>
       <div>
         {projects.map((project, index) => {
+          if (mobileView) {
+            return mobileViewMode(project);
+          }
           if (index % 2 === 0) {
             return rtlMode(project);
           }
