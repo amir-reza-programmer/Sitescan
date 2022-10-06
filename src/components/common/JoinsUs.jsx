07f044a0cc20dfc51d491383de376b9a566d01c0
AmirReza <amirrezazare130081@gmail.com>
2022-10-07 00:12:36 +0330

    join us button mobile

--- src/components/common/JoinsUs.jsx
+++ src/components/common/JoinsUs.jsx
@@ -1,11 +1,25 @@
 import styles from "./JoinUs.module.css";
+import { useSelector } from "react-redux";
 
 const JoinUs = () => {
+  const mobileView = useSelector((state) => state.mobile.mobileView);
   return (
     <div className={styles.join_us_section}>
-      <p className={styles.text}>می‌خواهید در این مسیر همراه ما باشید؟</p>
+      {mobileView ? (
+        <p className={styles.text_mobile}>
+          می‌خواهید در این مسیر <br />
+          همراه ما باشید؟
+        </p>
+      ) : (
+        <p className={styles.text}>می‌خواهید در این مسیر همراه ما باشید؟</p>
+      )}
+
       <a href="">
-      <button className={styles.btn}>به ما بپیوندید</button>
+        <button
+          className={styles.btn + ` ${mobileView ? styles.btn_mobile : null}`}
+        >
+          به ما بپیوندید
+        </button>
       </a>
     </div>
   );
