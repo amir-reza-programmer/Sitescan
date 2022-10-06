7b5d62573258d80c3a5bc1075942d7eed49aa910
AmirReza <amirrezazare130081@gmail.com>
2022-10-06 19:01:00 +0330

    mobile navbar

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,9 +1,37 @@
+import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
+import { useSelector } from "react-redux";
+import MenuButton from "../../images/Navbar/MenuButton.svg";
 import Icon from "../../images/SitescanIcon.svg";
 import IranFlag from "../../images/IranFlag.svg";
 
 const Navbar = ({ active }) => {
+  const mobileView = useSelector((state) => state.mobile.mobileView);
+  const menu = useRef();
+  const menuButton = useRef();
+  useEffect(() => {
+    /**
+     * Alert if clicked on outside of element
+     */
+    function handleClickOutside(event) {
+      if (
+        menu.current &&
+        menuButton.current &&
+        !menu.current.contains(event.target) &&
+        !menuButton.current.contains(event.target)
+      ) {
+        setIsOpen(false);
+      }
+    }
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [menu]);
+  const [isOpened, setIsOpen] = useState(false);
   const itemStyle = (name, color) => {
     if (active === name) {
       return {
@@ -21,82 +49,110 @@ const Navbar = ({ active }) => {
     }
     return {};
   };
-
+  useEffect(() => {
+    console.log("now isopened ", isOpened);
+  }, [isOpened]);
+  const blureShape = (name, color) => {
+    if (!mobileView) {
+      return (
+        <div className={styles.blure} style={itemStyle(name, color)}></div>
+      );
+    }
+    return null;
+  };
+  const clickDropDownLi = () => {
+    if (mobileView) {
+      setIsOpen(false);
+    }
+  };
+  const links = (
+    // <Collapse isOpened={isOpened}>
+    <div
+      ref={menu}
+      className={
+        mobileView
+          ? isOpened
+            ? styles.drop_down
+            : styles.drop_down__closed
+          : styles.nav__center
+      }
+    >
+      <ul>
+        <li onClick={clickDropDownLi}>
+          <div>
+            <Link style={itemColor("contact_us", "FF9712")} to="/contact_us">
+              تماس با ما
+            </Link>
+            {blureShape("contact_us", "FF9712")}
+          </div>
+        </li>
+        <li>
+          <div>
+            <Link style={itemColor("faq", "00FF87")} to="/faq">
+              سوالات متداول
+            </Link>
+            {blureShape("faq", "00FF87")}
+          </div>
+        </li>
+        <li>
+          <div>
+            <Link style={itemColor("projects", "FFE585")} to="/projects">
+              پروژه‌ها
+            </Link>
+            {blureShape("projects", "FFE585")}
+          </div>
+        </li>
+        <li>
+          <div>
+            <Link style={itemColor("about_us", "FF51EB")} to="/about_us">
+              درباره ما
+            </Link>
+            {blureShape("about_us", "FF51EB")}
+          </div>
+        </li>
+        <li>
+          <div>
+            <Link style={itemColor("home", "2EC5FF")} to="/home">
+              خانه
+            </Link>
+            {blureShape("home", "2EC5FF")}
+          </div>
+        </li>
+      </ul>
+    </div>
+    // </Collapse>
+  );
   return (
     <div className={styles["nav-parent"]}>
-      <nav className={styles.nav}>
-        <div className={styles.nav__right}>
+      <nav className={mobileView ? styles.nav_mobile : styles.nav}>
+        {mobileView ? (
+          <div className={styles.nav__right}>
+            <div className="icon">
+              {
+                <img
+                  onClick={() => {
+                    setIsOpen(isOpened ? false : true);
+                  }}
+                  ref={menuButton}
+                  src={MenuButton}
+                  alt=""
+                />
+              }
+            </div>
+          </div>
+        ) : null}
+        <div className={mobileView ? styles.nav__center : styles.nav__right}>
           <img src={Icon} alt="" />
           <ul>
             <li>سایت‌اسکن</li>
           </ul>
         </div>
-        <div className={styles.nav__center}>
-          <ul>
-            <li>
-              <div>
-                <Link
-                  style={itemColor("contact_us", "FF9712")}
-                  to="/contact_us"
-                >
-                  تماس با ما
-                </Link>
-                <div
-                  className={styles.blure}
-                  style={itemStyle("contact_us", "FF9712")}
-                ></div>
-              </div>
-            </li>
-            <li>
-              <div>
-                <Link style={itemColor("faq", "00FF87")} to="/faq">
-                  سوالات متداول
-                </Link>
-                <div
-                  className={styles.blure}
-                  style={itemStyle("faq", "00FF87")}
-                ></div>
-              </div>
-            </li>
-            <li>
-              <div>
-                <Link style={itemColor("projects", "FFE585")} to="/projects">
-                  پروژه‌ها
-                </Link>
-                <div
-                  className={styles.blure}
-                  style={itemStyle("projects", "FFE585")}
-                ></div>
-              </div>
-            </li>
-            <li>
-              <div>
-                <Link style={itemColor("about_us", "FF51EB")} to="/about_us">
-                  درباره ما
-                </Link>
-                <div
-                  className={styles.blure}
-                  style={itemStyle("about_us", "FF51EB")}
-                ></div>
-              </div>
-            </li>
-            <li>
-              <div>
-                <Link style={itemColor("home", "2EC5FF")} to="/home">
-                  خانه
-                </Link>
-                <div
-                  className={styles.blure}
-                  style={itemStyle("home", "2EC5FF")}
-                ></div>
-              </div>
-            </li>
-          </ul>
-        </div>
+        {links}
         <div className={styles.nav__left}>
           <ul>
             <li>
-              <span>فارسی</span> <img src={IranFlag} alt="iran flag" />
+              {!mobileView ? <span>فارسی</span> : null}
+              <img src={IranFlag} alt="iran flag" />
             </li>
           </ul>
         </div>
