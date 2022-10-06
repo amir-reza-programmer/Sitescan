7af1e08490609acad1028f9bab35c973725b7390
AmirReza <amirrezazare130081@gmail.com>
2022-10-07 01:45:18 +0330

    Alphabetic Order

--- src/data_information/teamMembers.js
+++ src/data_information/teamMembers.js
@@ -15,35 +15,40 @@ const teamMembers = [
   },
   {
     id: 1,
-    pic: Parisa,
-    fullname: "پریسا غیبی",
-    position: "Product manager",
-    linkedin: "https://www.linkedin.com/in/parisagheibi",
-    github: "",
-  },
-  {
-    id: 2,
-    pic: Nima,
-    fullname: "نیما نمازی",
-    position: "UI/UX designer",
-    linkedin: "http://www.linkedin.com/in/nimanamazi",
-    github: "",
-  },
-  {
-    id: 3,
     pic: AmirReza,
     fullname: "امیررضا زارع",
     position: "Front-end developer",
     linkedin: "https://www.linkedin.com/in/amirrezaa-zare/",
     github: "https://github.com/amir-reza-programmer",
+
+    
   },
   {
-    id: 4,
+    id: 2,
+    pic: Parisa,
+    fullname: "پریسا غیبی",
+    position: "Product manager",
+    linkedin: "https://www.linkedin.com/in/parisagheibi",
+    github: "",
+
+
+  },
+  {
+    id: 3,
     pic: Mehrab,
     fullname: "مهراب غیبی",
     position: "Front-end developer",
     linkedin: "https://www.linkedin.com/in/mehrab-gheibi-988bb3174/",
     github: "https://github.com/VigilantV",
+  },
+  {
+    id: 4,
+    pic: Nima,
+    fullname: "نیما نمازی",
+    position: "UI/UX designer",
+    linkedin: "http://www.linkedin.com/in/nimanamazi",
+    github: "",
+    
   },
 ];
 
